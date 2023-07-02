0857907524913adfce303d3dd7b13bf7789dd728
Brendan Cordova <cordovalegacy19@gmail.com>
2023-07-02 14:40:45 -0700

    Shortened Required Syntax for Print Statements

--- First.java
+++ First.java
@@ -1,7 +1,9 @@
+import static java.lang.System.out; // !allows us to shorten print statement
+
 public class First{ // !define class
 
     public static int addNums(int a, int b){ // !define return type
-        System.out.println(a+b); // !print statement
+        out.println(a+b); // !print statement
         return a + b; // !ensure return matches return type
     }
 
@@ -9,13 +11,13 @@ public class First{ // !define class
         String name = "Brendan"; 
         String age = "27";
         String hometown = "Henderson, NV";
-        System.out.println("My name is: " + name);
-        System.out.println("My age is: " + age);
-        System.out.println("My hometown is: " + hometown);
-        System.out.println(addNums(5, 10)); // !how to invoke functions
+        out.println("My name is: " + name);
+        out.println("My age is: " + age);
+        out.println("My hometown is: " + hometown);
+        out.println(addNums(5, 10)); // !how to invoke functions
 
         // !how to call main function
-        for(String var: args){System.out.println("This is a: " + var);}
+        for(String var: args){out.println("This is a: " + var);}
     }
 
 }
